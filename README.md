31cc3fab8e53a5c880eab9db0a5d1902a54c43b9
Liudmila <226585@niuitmo.ru>
2017-09-12 22:38:27 +0300

    server app is completed

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 https://kodaktor.ru/g/04092017
 
+клиентское приложение на вебпакбин: https://www.webpackbin.com/bins/-Ktqy_oH00U0fB28fKuP
---
+++ index.js
@@ -0,0 +1,29 @@
+import http from 'http';
+import url from 'url';
+import moment from 'moment';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'myname': 'Liudmila',
+        'Content-Type': 'text/plain; charset=utf-8'
+    });
+    const urlObj = url.parse(req.url);
+    switch (urlObj.pathname) {
+        case '/time':
+            const dateTime = moment().format('DD-MM-YY HH:mm');
+            res.end(dateTime);
+            break;
+        case '/author':
+            res.end('Людмила Букирёва');
+            break;
+        case '/length':
+            const queryLength = (urlObj.query ? urlObj.query.length : 0).toString();
+            res.end(queryLength);
+            break;
+        default:
+            res.end('This path is not provided');
+            break;
+    }
+});
+
+server.listen(8000, () => console.log("Server start on 8000"));
